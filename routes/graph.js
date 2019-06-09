75adbcaac8fdf028d2c9f0773129093535537c50
ppcamp <p.augustocampos@gmail.com>
2019-06-09 11:15:23 -0300

    final -- incompleto

---
+++ routes/graph.js
@@ -0,0 +1,22 @@
+const con = require('../routes/db');
+
+exports.list = function(req, res) {
+    console.log('listing..');
+    con.connect((err) => {
+        con.query('SELECT titulo,texto,logins_nick FROM conteudo ORDER BY id_conteudo DESC;', (err, sqlres) => {
+            if (err) console.log("Erro: %s", err);
+            // console.log('\t\t\tResultado:\n');
+            // sqlres.forEach(element => {
+            //     console.log('Titulo:\t%s', element.titulo);
+            //     console.log('Texto:\t%s\n\n', element.texto);
+            //     console.log('-----------------');
+            // });
+            res.render(
+                'profile.ejs', {
+                    nome_tabela: "conteudo",
+                    data: sqlres
+                });
+            console.log('rendering');
+        });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const con = require('./db');
 const tb = require('./list');
+// const tbb = require('./graph.js');
 
 // Login Page or Main page
 router.get('/login', function(req, res, next) {
@@ -16,18 +17,16 @@ router.get('/home', function(req, res, next) {
     if (!global.logged) {
         return res.redirect('/login');
     } else {
-        console.log("entrou");
         tb.list(req, res);
     }
 });
-// router.get('/home', tb.list);
 // Profile Page
-// router.get('/profile', function(req, res, next) {
-//     if (!global.logged) {
-//         return res.redirect('/login');
-//     }
-//     return res.render('./pages/profile.ejs');
-// });
+router.get('/profile', function(req, res, next) {
+    if (!global.logged) {
+        return res.redirect('/login');
+    }
+    return res.render('profile.ejs');
+});
 // Store page
 router.get('/store', function(req, res, next) {
     if (!global.logged) {
@@ -41,7 +40,14 @@ router.get('/insert', (req, res, next) => {
         return res.redirect('/login');
     }
     return res.render('insert.ejs');
-})
+});
+// Help page
+router.get('/help', (req, res, next) => {
+    if (!global.logged) {
+        return res.redirect('/login');
+    }
+    return res.render('help.ejs');
+});
 
 // EXIT
 router.get('/exit', (req, res, next) => {
